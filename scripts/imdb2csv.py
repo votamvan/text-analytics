8f38fcc60cd2ca1b78c78ded98046cb32d1706a3
Van Vo Tam <vanvt@anymindgroup.com>
2019-04-19 15:54:39 +0700

    imdb2csv

--- scripts/imdb2csv.py
+++ scripts/imdb2csv.py
@@ -4,6 +4,7 @@ import html
 import re
 import os
 import numpy as np
+import pandas as pd
 
 DO_DOWNLOAD = False
 if DO_DOWNLOAD:
@@ -36,3 +37,21 @@ def get_texts(path):
 
 trn_texts,trn_labels = get_texts(PATH/'train')
 val_texts,val_labels = get_texts(PATH/'test')
+print("trn_texts", len(trn_texts), "val_texts", len(val_texts))
+
+col_names = ['labels','text']
+np.random.seed(42)
+trn_idx = np.random.permutation(len(trn_texts))
+val_idx = np.random.permutation(len(val_texts))
+trn_texts = trn_texts[trn_idx]
+val_texts = val_texts[val_idx]
+
+trn_labels = trn_labels[trn_idx]
+val_labels = val_labels[val_idx]
+df_trn = pd.DataFrame({'text':trn_texts, 'labels':trn_labels}, columns=col_names)
+df_val = pd.DataFrame({'text':val_texts, 'labels':val_labels}, columns=col_names)
+df_trn[df_trn['labels']!=2].to_csv('imdb-train.csv', header=False, index=False)
+df_val.to_csv('imdb-test.csv', header=False, index=False)
+
+with open('imdb-classes.txt', 'w', encoding='utf-8') as f:
+    f.writelines(f'{o}\n' for o in CLASSES)
